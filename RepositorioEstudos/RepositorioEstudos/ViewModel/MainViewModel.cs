a762b407fd5502ef6f3daeea62461cdd7629103d
Jardel Oliveira <jardell.oliveira@hotmail.com>
2021-09-09 14:57:48 -0300

    Adicionar arquivos de projeto.

---
+++ RepositorioEstudos/RepositorioEstudos/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using RepositorioEstudos.View;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RepositorioEstudos.ViewModel
+{
+    public class MainViewModel
+    {
+        #region Campos
+        public ICommand QrCodeCommand => new Command(async () =>
+        {
+            
+            await App.Current.MainPage.Navigation.PushAsync(new QrCodeView());
+        });
+        #endregion
+    }
+}
---
+++ RepositorioEstudos/RepositorioEstudos/ViewModel/QrCodeViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace RepositorioEstudos.ViewModel
+{
+    public class QrCodeViewModel
+    {
+        #region Campos
+        
+        public ICommand ScannerCommand => new Command(async () =>
+        {
+            await Abrir();
+        });
+        #endregion
+        #region Métodos
+        private async Task Abrir()
+        {
+           
+
+            var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+            var result = await scanner.Scan();
+            if(result != null)
+            {
+                if(await Launcher.CanOpenAsync(new Uri(result.Text)))
+                    await Launcher.OpenAsync(new Uri(result.Text));
+            }
+           
+
+        }
+        #endregion
+    }
+}
