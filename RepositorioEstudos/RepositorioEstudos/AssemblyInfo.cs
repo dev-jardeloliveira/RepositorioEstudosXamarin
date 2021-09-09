d956a2d7a731bb7c6f479811db80861848dc8f04
Jardel Oliveira <jardell.oliveira@hotmail.com>
2021-09-09 16:33:46 -0300

    Fontes

--- RepositorioEstudos/RepositorioEstudos/AssemblyInfo.cs
+++ RepositorioEstudos/RepositorioEstudos/AssemblyInfo.cs
@@ -1,3 +1,13 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+//Fontes
+[assembly: ExportFont("Brightholiday.ttf", Alias = "Brightholiday")]
+[assembly: ExportFont("Best School.otf", Alias = "Best School")]
+[assembly: ExportFont("Hamberger-Bold.ttf", Alias = "Hamberger-Bold")]
+[assembly: ExportFont("KGEverSinceNewYork.ttf", Alias = "KGEverSinceNewYork")]
+[assembly: ExportFont("Font Awesome 5 Brands-Regular-400.otf", Alias = "Brands-Regular-400")]
+[assembly: ExportFont("Font Awesome 5 Free-Regular-400.otf", Alias = "Regular-400")]
+[assembly: ExportFont("Font Awesome 5 Free-Solid-900.otf", Alias = "Solid-900")]
--- RepositorioEstudos/RepositorioEstudos/ViewModel/MainViewModel.cs
+++ RepositorioEstudos/RepositorioEstudos/ViewModel/MainViewModel.cs
@@ -8,9 +8,12 @@ namespace RepositorioEstudos.ViewModel
     {
         #region Campos
         public ICommand QrCodeCommand => new Command(async () =>
-        {
-            
+        {            
             await App.Current.MainPage.Navigation.PushAsync(new QrCodeView());
+        });
+        public ICommand FontCommand => new Command(async () =>
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new FontesView());
         });
         #endregion
     }
